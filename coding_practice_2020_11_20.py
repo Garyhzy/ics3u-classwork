a7881549a860d4a5cf4942cffd046dd907573fa5
Garyhzy <74619233+Garyhzy@users.noreply.github.com>
2020-11-20 13:14:04 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,3 +1,17 @@
+practice 1
+
 input("what's your favorite color")
 
 color = (input("my favourite color is also blue"))
+
+
+
+
+practice 2 
+
+cans = int(input("How many cans are in a pack: "))
+pack = int(input("How many packs are there: "))
+
+Total = cans  * pack
+
+print(f"there are total of {Total} cans.")
