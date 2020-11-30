e056e88aaec70649d547ad56ee79f7f5db1efd5f
Garyhzy <74619233+Garyhzy@users.noreply.github.com>
2020-11-30 00:05:03 -0500

    Create coding_practice_2020_11_27.py

---
+++ coding_practice_2020_11_27.py
@@ -0,0 +1,60 @@
+
+i = -339
+while i < -80:
+  i+=7
+  print (i)
+print ("done")
+
+i = 308
+while i < 476:
+  i+=7
+  print (i)
+print ("done")
+
+i = 77
+while i < 152:
+  i+=6
+  print (i)
+print ("done")
+
+i = 338
+while i < 338:
+  i-=0
+  print (i)
+print ("done")
+
+i = 91
+while i < 287:
+  i+=7
+  print (i)
+print ("done")
+
+i = -425
+while i > -317:
+  i+=4
+  print (i)
+print ("done")
+
+i = 182
+while i < 452:
+  i+=10
+  print (i)
+print ("done")
+
+i = 84
+while i > 444:
+  i+=18
+  print (i)
+print ("done")
+
+i = -360
+while i > -110:
+  i+=10
+  print (i)
+print ("done")
+
+i = -70
+while i < 26:
+  i+=4
+  print (i)
+print ("done")
